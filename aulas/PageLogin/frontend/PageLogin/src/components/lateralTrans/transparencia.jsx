d8bbd0122f2d91271b37c0fe81305ac71d500fc6
Eduardo Fonseca <eduardofonseca0210@gmail.com>
2022-08-23 19:14:11 -0300

    commit

--- aulas/PageLogin/frontend/PageLogin/src/components/lateralTrans/transparencia.jsx
+++ aulas/PageLogin/frontend/PageLogin/src/components/lateralTrans/transparencia.jsx
@@ -2,22 +2,20 @@ import React from "react";
 import './transparencia.css'
 
 const Trans = () => {
-    return <div className="transContainer">
-        <div className="displayContainer">
-            <div className="tituloContainer">
-                <h1>Efetue Seu Login <br></br> Abaixo</h1>
-            </div>
-            <div className="caixaLoginContainer">
+    return <div className="displayContainer">
+        <div className="loginContainer">
+            <h1 className="tituloContainer">Efetue Seu Login<br></br>Abaixo</h1>
+            <div className="inputsContainer">
                 <div className="loginContainer">
-                    <p>Login:</p>
-                    <input className="loginInput" type="text" placeholder="Digite seu login aqui.." />
+                    <p>LOGIN</p>
+                    <input className="textInput" type="text" placeholder="Digite seu login aqui.."/>
                 </div>
                 <div className="senhaContainer">
-                    <p>Senha:</p>
-                    <input className="senhaInput" type="password" placeholder="Digite sua senha aqui.."/>
+                    <p>SENHA</p>
+                    <input className="passwordInput" type="password" placeholder="Digite sua senha aqui.."/>
                 </div>
                 <div className="btnContainer">
-                    <button className="btn" type="submit">Entrar</button>
+                    <button className="btn" type="submit">ENTRAR</button>
                 </div>
             </div>
         </div>
